172c9dbed030f01ca2fa6f6dc40dc20633d5ed04
Wilton Garcia <wiltongarciaweb@gmail.com>
2021-05-07 22:57:49 -0300

    Criando componente de background

--- Caoversor de Idades/APP/MODULES/AgeConverter/View/AgeConverterView.swift
+++ Caoversor de Idades/APP/MODULES/AgeConverter/View/AgeConverterView.swift
@@ -5,4 +5,31 @@
 //  Created by Wilton Garcia on 07/05/21.
 //
 
-import Foundation
+import UIKit
+
+final class AgeConverterView: UIView{
+    
+    private let viewBackground : UIView = {
+        let viewBackground = UIView()
+        viewBackground.backgroundColor = .blue
+        viewBackground.translatesAutoresizingMaskIntoConstraints = true
+        return viewBackground
+    }()
+    
+    private func setupBackround(){
+        addSubview(viewBackground)
+        
+        NSLayoutConstraint.activate([
+            viewBackground.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            viewBackground.heightAnchor.constraint(equalTo: heightAnchor),
+            viewBackground.leadingAnchor.constraint(equalTo: leadingAnchor),
+            viewBackground.trailingAnchor.constraint(equalTo: trailingAnchor)
+        ])
+    }
+    
+    convenience init(){
+        self.init(frame:.zero)
+        setupBackround()
+    }
+    
+}
